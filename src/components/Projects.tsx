9a4eb96d9449fb4c25313710e33fb71d5a18e64e
David Pang <dp7134@gmail.com>
2024-09-03 17:25:13 -0400

    trying to fix get algorithm visualizer to appear

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "../App.css";
 
 const Projects = () => {
@@ -21,7 +21,7 @@ const Projects = () => {
       {isProjectVisible && (
         <iframe
           className="sorting-visualizer"
-          src="/dist/index.html"
+          src={`${import.meta.env.VITE_PUBLIC_URL}/dist/index.html`}
           title="Sorting Algorithm Visualizer"
           style={{
             width: "100%",
